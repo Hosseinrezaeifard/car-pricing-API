b1bec5b2ebc312b14df61f2c8779e1d93905c5e5
Hossein Rezaei Fard <57575843+Hosseinrezaeifard@users.noreply.github.com>
2024-02-05 04:33:07 -0800

    Fix naming of entity files

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,8 +4,8 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
 import { ReportsModule } from './reports/reports.module';
-import { UserEntity } from './users/users.entity';
-import { ReportEntity } from './reports/reports.entity';
+import { UserEntity } from './users/user.entity';
+import { ReportEntity } from './reports/report.entity';
 
 @Module({
   imports: [
--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -2,7 +2,7 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { UsersController } from './users.controller';
 import { UsersService } from './users.service';
-import { UserEntity } from './users.entity';
+import { UserEntity } from './user.entity';
 
 @Module({
   imports: [TypeOrmModule.forFeature([UserEntity])],
