06b920434cdfc004e365445ee854e806d970e17f
Hossein Rezaei Fard <57575843+Hosseinrezaeifard@users.noreply.github.com>
2024-02-07 17:23:06 +0330

    wrap the interceptor in a simpler decorator

--- src/interceptors/serialize.interceptor.ts
+++ src/interceptors/serialize.interceptor.ts
@@ -7,6 +7,11 @@ import {
 import { Observable } from 'rxjs';
 import { map } from 'rxjs/operators';
 import { plainToClass } from 'class-transformer';
+
+export function Serialize(dto: any) {
+  return UseInterceptors(new SerializeInterceptor(dto));
+}
+
 export class SerializeInterceptor implements NestInterceptor {
   constructor(private dto: any) {}
   intercept(context: ExecutionContext, handler: CallHandler): Observable<any> {
--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -13,8 +13,9 @@ import {
 import { CreateUserDto } from './dtos/create-user-dto';
 import { UpdateUserDto } from './dtos/update-user-dto';
 import { UsersService } from './users.service';
-import { SerializeInterceptor } from 'src/interceptors/serialize.interceptor';
+import { Serialize } from 'src/interceptors/serialize.interceptor';
 import { UserDto } from './dtos/user.dto';
+
 @Controller('auth')
 export class UsersController {
   constructor(private usersService: UsersService) {}
@@ -24,10 +25,8 @@ export class UsersController {
     this.usersService.create(body.email, body.password);
   }
 
-  // @UseInterceptors(ClassSerializerInterceptor)
-  @UseInterceptors(new SerializeInterceptor(UserDto))
+  @Serialize(UserDto)
   @Get('/:id')
-  // Ids are numbers in the request params even though they're not strings (are numbers) in db!
   async findUser(@Param('id') id: string) {
     const user = await this.usersService.findOne(parseInt(id));
     if (!user) {
