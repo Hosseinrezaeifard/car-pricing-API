6583f8ea8174b247feff6c1a2aaa5e290734dbb1
Hossein Rezaei Fard <57575843+Hosseinrezaeifard@users.noreply.github.com>
2024-02-09 02:20:11 +0330

    implement sign up

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -8,6 +8,6 @@ import { AuthService } from './auth.service';
 @Module({
   imports: [TypeOrmModule.forFeature([UserEntity])],
   controllers: [UsersController],
-  providers: [UsersService],
+  providers: [UsersService, AuthService],
 })
 export class UsersModule {}
