de009c77690cedd6377a157ea3fc97617c2c75a2
Hossein Rezaei Fard <57575843+Hosseinrezaeifard@users.noreply.github.com>
2024-02-09 00:06:08 +0330

    Initialize authentication service

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -3,6 +3,7 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { UsersController } from './users.controller';
 import { UsersService } from './users.service';
 import { UserEntity } from './user.entity';
+import { AuthService } from './auth.service';
 
 @Module({
   imports: [TypeOrmModule.forFeature([UserEntity])],
